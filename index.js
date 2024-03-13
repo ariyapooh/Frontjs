c73a005cc2c83301e56a0cb76876eefa76518dde
Your Name <6406021621137@fitm.kmutnb.ac.th>
2024-03-14 00:34:58 +0700

    Testruk

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 
 //varible for localhost 3000 
-const base_url = "http://localhost:3000";
+const base_url = "node60211-env-2576636.proen.app.ruk-com.cloud";
 
 app.set("views",path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
